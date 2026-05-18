e9dc72d3cbb1a5265f849f876b909c34fd484d0e
Wahae <faithnyandiko12@gmail.com>
2026-05-18 10:47:38 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'epsilon-sports-v1';
+const assets = ['./site.html', './manifest.json'];
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(assets);
+    }));
+});
+self.addEventListener('fetch', (event) => {
+    event.respondWith(caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+    }));
+});
